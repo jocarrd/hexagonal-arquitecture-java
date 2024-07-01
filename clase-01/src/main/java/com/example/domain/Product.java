fdc04cf88a515768100622e2e69a91c1fa8b749e
Jorge Carrera <jorgecdi@ext.inditex.com>
2024-07-01 13:11:10 +0200

    first commit

---
+++ clase-01/src/main/java/com/example/domain/Product.java
@@ -0,0 +1,34 @@
+package com.example.domain;
+
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    
+    public double getPrice() {
+        return price;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
